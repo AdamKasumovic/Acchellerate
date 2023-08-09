208e0e2dd6650eacdd0f164f5ec04a8df6b17740
AdamKasumovic <kasumovi@msu.edu>
2023-08-09 17:11:11 -0400

    Adding chunk 76 of files

---
+++ MI498-SP23-Bungie/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,101 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID MUSICEVENT = 3341137194U;
+        static const AkUniqueID VICTORYMUSICEVENT = 2738305854U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSICSTATE
+        {
+            static const AkUniqueID GROUP = 1021618141U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ARANK = 1216448900U;
+                static const AkUniqueID ARANKLEVELCOMPLETE = 1570785891U;
+                static const AkUniqueID BRANK = 153585799U;
+                static const AkUniqueID BRANKLEVELCOMPLETE = 3689082510U;
+                static const AkUniqueID CRANK = 2729331502U;
+                static const AkUniqueID CRANKLEVELCOMPLETE = 2805944725U;
+                static const AkUniqueID DEFAULT = 782826392U;
+                static const AkUniqueID DEFAULTLEVELCOMPLETE = 2674757319U;
+                static const AkUniqueID DRANK = 2740132969U;
+                static const AkUniqueID MAINMENU = 3604647259U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID ZRANK = 3195934671U;
+                static const AkUniqueID ZRANKLEVELCOMPLETE = 3649428758U;
+            } // namespace STATE
+        } // namespace MUSICSTATE
+
+        namespace VICTORYMUSICSTATE
+        {
+            static const AkUniqueID GROUP = 1688184833U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ARANK = 1216448900U;
+                static const AkUniqueID BRANK = 153585799U;
+                static const AkUniqueID CRANK = 2729331502U;
+                static const AkUniqueID DEFAULT = 782826392U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID ZRANK = 3195934671U;
+            } // namespace STATE
+        } // namespace VICTORYMUSICSTATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MUSICSWITCH
+        {
+            static const AkUniqueID GROUP = 1445037870U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ALL = 1100754030U;
+                static const AkUniqueID DEFAULT = 782826392U;
+            } // namespace SWITCH
+        } // namespace MUSICSWITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SPEED = 640949982U;
+        static const AkUniqueID VOLUME = 2415836739U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
