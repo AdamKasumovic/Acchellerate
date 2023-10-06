186c49af3351b565c1b32456cf12043fdbd58668
AdamKasumovic <kasumovi@msu.edu>
2023-10-06 00:18:53 -0400

    fly mission working

---
+++ MI498-SP23-Bungie/Assets/_Scripts/Missions/FlyMission.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// SingleMission that requires flying (airborne boosting) for a certain amount of time.
+/// </summary>
+public class FlyMission : SingleMission
+{
+    public float Airtime { get; private set; }
+
+    [Header("Fly Mission Settings")]
+    public float RequiredTime = 10.0f;
+
+    private CarManager instance;
+    private RCC_CarControllerV3 carController;
+
+    protected override void Start()
+    {
+        base.Start();
+        Airtime = 0;
+        instance = CarManager.Instance;
+        carController = instance.carController;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (!carController.isGrounded && instance.boost && instance.horBoostTimer > 0 && !instance.boostRefreshing)
+        {
+            Execute();
+        }
+
+        string progress = $"{Airtime:0.0}s/{RequiredTime:0.0}s";
+        string timer = UseTimer ? $" Time left: {Mathf.Max(0, timeRemaining):0.0}s" : "";
+
+        MissionName = $"Fly for {RequiredTime:0.0} total seconds ({progress}).{timer}";
+        Debug.Log(MissionName);
+    }
+
+    public override void Execute()
+    {
+        Airtime += Time.deltaTime;
+        if (Airtime >= RequiredTime)
+        {
+            CompleteMission();
+        }
+    }
+
+    public override void FailMission()
+    {
+        base.FailMission();  // MUST CALL THIS!
+        Debug.Log("Fly Mission Failed!");
+    }
+
+    public override void CompleteMission()
+    {
+        base.CompleteMission();  // MUST CALL THIS!
+        Debug.Log("Fly Mission Completed!");
+    }
+}
